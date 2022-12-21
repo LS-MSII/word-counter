15e3d0454072351f9ccd0fa3a5eb6f0d4defe1f0
Víctor Caballero <victor.caballero@salle.url.edu>
2022-12-21 11:42:54 +0100

    Change Boolean to boolean

--- src/model/domain/filter/concrete-filter/VowelStartFilter.ts
+++ src/model/domain/filter/concrete-filter/VowelStartFilter.ts
@@ -2,7 +2,7 @@ import {Filter} from "../Filter";
 import {Word} from "../../Word";
 
 export class VowelStartFilter implements Filter {
-    public apply(word: Word): Boolean {
+    public apply(word: Word): boolean {
         const vowelsToTest = ["a", "e", "i", "o", "u"]
         const wordFirstCharacter = word.getRaw().at(0)
 
