a9c39ca3d3223678da7e2d7188416d903f1150b4
Víctor Caballero <victor.caballero@salle.url.edu>
2022-12-21 11:46:21 +0100

    Add scope to constructor and apply

--- src/model/domain/filter/concrete-filter/MoreThanLengthFilter.ts
+++ src/model/domain/filter/concrete-filter/MoreThanLengthFilter.ts
@@ -4,11 +4,11 @@ import {Word} from "../../Word";
 export class MoreThanLengthFilter implements Filter {
     private wordLengthToTest: number
 
-    constructor(length: number) {
+    public constructor(length: number) {
         this.wordLengthToTest = length
     }
 
-    apply(word: Word): boolean {
+    public apply(word: Word): boolean {
         return word.getRaw().length > this.wordLengthToTest;
     }
 
